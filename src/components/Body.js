35450fca14fcc563301ec2385030d1fe832fe61a
Liqiq <illa77mucolajovuch@gmail.com>
2025-11-29 22:51:26 +0200

    added myself

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/baby.jpg"} alt="Illia" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Illia Yavorskyi</h2>
+          <p style={styles.position}>QA</p>
         </div>
       </div>
     </main>
