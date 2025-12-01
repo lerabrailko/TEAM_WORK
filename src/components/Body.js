3b54fda2426830165e2eb1b6629bc0bb4aab515b
lerabrailko <lerabrailkos@gmail.com>
2025-12-01 08:40:28 +0200

    problem branch

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
     return (
         <main style={styles.body}>
             <div style={styles.card}>
-                <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+                <img src={"/avatar-default.png"} alt="Casandra Amarylys" style={styles.photo} />
                 <div style={styles.textContainer}>
-                    <h2 style={styles.name}>John Doe</h2>
-                    <p style={styles.position}>Software engineer</p>
+                    <h2 style={styles.name}>Casandra Amarylys</h2>
+                    <p style={styles.position}>DevOps</p>
                 </div>
             </div>
         </main>
